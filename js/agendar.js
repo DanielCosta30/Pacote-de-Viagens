fdefef5b36c31314687fb5fbad82df87786c3868
DanielCosta30 <nielcosta24@gmail.com>
2022-09-02 16:18:51 -0300

    feat(pageagendar-css): primeira linha de codigo

--- js/agendar.js
+++ js/agendar.js
@@ -1,6 +1,73 @@
-var entralogin = document.querySelector(".acesso");
+var btn = document.querySelector('#vercfp')
 
-entralogin.addEventListener("click", function (event) {
-  event.preventDefault();
+var nome = document.querySelector('#nome')
+var labelnome = document.querySelector('#labelnome')
+var validnome = false
+
+var cpf = document.querySelector('#cpf')
+var labelcpf = document.querySelector('#labelcpf')
+var validcpf = false
+
+var email = document.querySelector('#email')
+var labelemail = document.querySelector('#labelemail')
+var validemail = false
+
+var destino = document.querySelector('#destino')
+var labeldestino = document.querySelector('#labeldestino')
+var validdestino = false
+
+var data = document.querySelector('#data')
+var labeldata = document.querySelector('#labeldata')
+var validdata = false
+
+var contato = document.querySelector('#contato')
+var labelcontato = document.querySelector('#labelcontato')
+var validcontato = false
+
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
+
+/* validação dos input---->*/
+
+nome.addEventListener('keyup', () => {
+  if(nome.value.length <= 5){
+  labelnome.setAttribute('style', 'color: red ')
+  labelnome.innerHTML = 'Nome *inválido'
+  validnome = false
+ 
+} else {
+  labelnome.setAttribute('style', 'color: #000 ')
+  labelnome.innerHTML ='Nome:'
+  validemail = true
+}
 
 })
+
+email.addEventListener('keyup', () => {
+  if(email.value.length <= 5){
+  labelemail.setAttribute('style', 'color: red ')
+  labelemail.innerHTML = 'email *Email inválido'
+  validemail = false
+ 
+} else {
+  labelemail.setAttribute('style', 'color: #000 ')
+  labelemail.innerHTML ='Email:'
+  validemail = true
+}
+
+})
+
+confirmsenha.addEventListener('keyup', () => {
+  if(senha.value != confirmsenha.value){
+  labelconfirmsenha.setAttribute('style', 'color: red ')
+  labelconfirmsenha.innerHTML = 'Confirma sua senha *Não conferem'
+  validconfirmsenha = false
+ 
+  } else {
+  labelconfirmsenha.setAttribute('style', 'color: #000 ')
+  labelconfirmsenha.innerHTML ='Confirme sua senha :'
+  validconfirmsenha = true      
+  }   
+})
+
+/*  <------*/
