2f6a415ce8b276e80408dd220c86f3284639921c
DanielCosta30 <nielcosta24@gmail.com>
2022-09-02 16:11:47 -0300

    feat(pageconsulta-js): queryslector para consulta

--- js/consulta.js
+++ js/consulta.js
@@ -1,6 +1,28 @@
-var entralogin = document.querySelector(".acesso");
+var btn = document.querySelector('#vercfp')
 
-entralogin.addEventListener("click", function (event) {
-  event.preventDefault();
+var nome = document.querySelector('#nome')
+var labelnome = document.querySelector('#labelnome')
+var validnome = false
 
-})
+var cpf = document.querySelector('#cpf')
+var labelcpf = document.querySelector('#labelcpf')
+var validcpf = false
+
+var email = document.querySelector('#email')
+var labelemail = document.querySelector('#labelemail')
+var validemail = false
+
+var destino = document.querySelector('#destino')
+var labeldestino = document.querySelector('#labeldestino')
+var validdestino = false
+
+var data = document.querySelector('#data')
+var labeldata = document.querySelector('#labeldata')
+var validdata = false
+
+var contato = document.querySelector('#contato')
+var labelcontato = document.querySelector('#labelcontato')
+var validcontato = false
+
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
