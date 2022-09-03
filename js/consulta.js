56bf26ffbe9f7c8578a100e0762b09b9b081e31b
DanielCosta30 <nielcosta24@gmail.com>
2022-09-03 03:12:41 -0300

    feat(pageconsultar-js)add linha de cod consultar

--- js/consulta.js
+++ js/consulta.js
@@ -14,6 +14,24 @@ let msgError = document.querySelector('#msgError')
 let msgSuccess = document.querySelector('#msgSuccess')
 
 
+/* acessar agendamento */
 
+let consultarag = document.querySelector(".consultar");
 
+consultarag.addEventListener("click", function (event) {
+  event.preventDefault();
 
+  let nome = document.querySelector("#nome");
+  let labelnome = document.querySelector("#labelnome");
+
+  let cpf = document.querySelector("#cpf");
+  let labelcpf = document.querySelector("#labelcpf");
+
+  let msgError = document.querySelector("#msgError");
+  let listauser = [];
+
+  let uservalid = {
+    nome: "",
+    cpf: "",
+  };
+  listauser = JSON.parse(localStorage.getItem("listauser"));
