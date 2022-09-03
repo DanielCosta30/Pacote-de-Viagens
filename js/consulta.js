3157fc7ae115a79bf5bfa959b5d4ce44f039aaba
DanielCosta30 <nielcosta24@gmail.com>
2022-09-03 03:03:13 -0300

    feat(pageconsulta-js)alterado "var " p/  "let"

--- js/consulta.js
+++ js/consulta.js
@@ -1,1 +1,19 @@
 var btn = document.querySelector('#vercpf')
+
+/* verificar agendamento---->*/
+
+let email = document.querySelector('#email')
+let labelemail = document.querySelector('#labelemail')
+let validemail = false
+
+let senha = document.querySelector('#senha')
+let labelsenha = document.querySelector('#labelSenha')
+let validsenha = false
+
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
+
+
+
+
+
